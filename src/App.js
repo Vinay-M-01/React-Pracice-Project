ac9c193144e3f6ba0b1cb1205504a6e9a12bdf7f
Vinay-M-01 <vinaydm1011@gmail.com>
2022-06-29 17:21:55 +0530

    Looping ExpenseItem List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Food",
+      amount: 100,
+      date: new Date(2020, 7, 14),
+      location: "President, Hubli",
+    },
+
+    { id: "e2",
+     title: "Petrol", 
+     amount: 200, 
+     date: new Date(2021, 2, 12),
+     location: "Shell, Unkal",
+    },
+     
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 300,
+      date: new Date(2021, 2, 28),
+      location: "Hyundai, BRK",
+    },
+    {
+      id: "e4",
+      title: "Party",
+      amount: 400,
+      date: new Date(2021, 5, 12),
+      location: "Hans, Vidyanagar",
+    },
+  ];
+
   return (
     <div>
+
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <div>
+      {expenses.map(expenses => {
+        return (
+          <ExpenseItem title={expenses.title} 
+          amount={expenses.amount}
+          date = {expenses.date}
+          location = {expenses.location}>
+          </ExpenseItem>
+        )
+      })};
+      </div>
+
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,17 @@
-function ExpenseItem(){
+import './ExpenseItm.css'
+
+function ExpenseItem(props){
+
     return(
-      <div>
-        <div>
-          <h2>Expense Item</h2>
+      <div  className="expense-item">
+        <div>{props.date.toISOString()}</div>
+
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <h3 className='expense-item__location'>Location: {props.location}</h3>
+
           <div>
-            <h3>Food Rs 10</h3>
-            <h3>Petrol Rs 100</h3>
-            <h3>Movies Rs 200</h3>
+            <h3 className='expense-item__price'>Rs: {props.amount}</h3>
           </div>
           
         </div>
