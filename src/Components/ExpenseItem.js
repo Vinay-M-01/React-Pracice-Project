8edea872d69dafa36ec39215afa2c801619dece2
Vinay-M-01 <vinaydm1011@gmail.com>
2022-06-29 13:32:15 +0530

    Added ExpenseItem details

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem(){
+    return(
+      <div>
+        <div>
+          <h2>Expense Item</h2>
+          <div>
+            <h3>Food Rs 10</h3>
+            <h3>Petrol Rs 100</h3>
+            <h3>Movies Rs 200</h3>
+          </div>
+          
+        </div>
+      </div>
+  );
+}
+  export default ExpenseItem;
