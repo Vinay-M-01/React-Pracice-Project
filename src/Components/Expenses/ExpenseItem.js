053d872eb4b59c7a36c240332b8ad375e2a7df0f
Vinay-M-01 <vinaydm1011@gmail.com>
2022-06-30 12:47:13 +0530

    Adding Event Handler

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -30,6 +30,14 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Clicked!!');
+  }
+
+  const deleteHandler = () => {
+    console.log('Element Delted!!');
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -37,6 +45,9 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
+
     </Card>
   );
 }
